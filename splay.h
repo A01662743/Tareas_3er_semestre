c435e62e8795d2b4a76750715bb952ab475533a2
Alejandro Contreras <A01662743@tec.mx>
2025-12-03 23:24:38 -0600

    splay.h

--- splay.h
+++ splay.h
@@ -1,35 +1,33 @@
-#ifndef SplayTree_H_
-#define SplayTree_H_
+#ifndef SPLAY_H_
+#define SPLAY_H_
 
 #include <string>
 #include <sstream>
 #include <iostream>
-using namespace std;
 
 template <class T> class SplayTree;
 
 template <class T>
 class Node {
 private:
-	T value;
-	Node *left, *right, *parent;
+    T value;
+    Node *left, *right, *parent;
 
-	Node<T>* succesor();
-	Node<T>* rot_right(Node<T>*);
-	Node<T>* rot_left(Node<T>*);
+    Node<T>* succesor();
+    Node<T>* rot_right(Node<T>*);
+    Node<T>* rot_left(Node<T>*);
+    Node<T>* splay(Node<T>*, Node<T>*);
 
 public:
-	Node(T);
-	Node(T, Node<T>*, Node<T>* ,Node<T>*);
-	Node<T>* add(T);
-	Node<T>* find(T);
-	void removeChilds();
-	void inorder(std::stringstream&) const;
-	void print_tree(std::stringstream&) const;
-	void preorder(std::stringstream&) const;
-	Node<T>* splay(Node<T>*, Node<T>*);
+    Node(T);
+    Node(T, Node<T>*, Node<T>*, Node<T>*);
+    Node<T>* add(T);
+    Node<T>* find(T);
+    Node<T>* remove(T);
+    void inorder(std::stringstream&) const;
+    void preorder(std::stringstream&) const;
 
-	friend class SplayTree<T>;
+    friend class SplayTree<T>;
 };
 
 template <class T>
@@ -37,222 +35,248 @@ Node<T>::Node(T val) : value(val), left(0), right(0), parent(0) {}
 
 template <class T>
 Node<T>::Node(T val, Node<T> *le, Node<T> *ri, Node<T> *p)
-	: value(val), left(le), right(ri), parent(p) {}
+    : value(val), left(le), right(ri), parent(p) {}
 
 template <class T>
 Node<T>* Node<T>::add(T val) {
-	if (value > val){
-		if (left == 0){
-			left = new Node(val);
-			left->parent = this;
-			return left;
-		}
-		else{
-			return left->add(val);
-		}
-	}
-	else {
-		if (right == 0){
-			right = new Node(val);
+    if (val < value) {
+        if (left != 0) {
+            return left->add(val);
+        } else {
+            left = new Node<T>(val);
+            left->parent = this;
+            return left;
+        }
+    } else {
+        if (right != 0) {
+            return right->add(val);
+        } else {
+            right = new Node<T>(val);
             right->parent = this;
-			cout << "right value: " << right->value;
-			return right;
-		}
-		else{
-			return right->add(val);
-		}
-	}
-	return 0;
+            return right;
+        }
+    }
 }
 
 template <class T>
 Node<T>* Node<T>::find(T val) {
-    if (val == value){ return this; }
-	else if (value > val){
-		if (left != 0){ return left->find(val); }
-	}
-	else{
-		if (right != 0){ return right->find(val); }
-	}
-	return 0;
+    if (val == value) {
+        return this;
+    } else if (val < value) {
+        if (left != 0)
+            return left->find(val);
+        else
+            return this;
+    } else {
+        if (right != 0)
+            return right->find(val);
+        else
+            return this;
+    }
 }
 
 template <class T>
 Node<T>* Node<T>::succesor() {
-	Node<T> *le, *ri;
+    Node<T> *p = this;
+    Node<T> *c = this->right;
 
-	le = left;
-	ri = right;
+    if (c == 0) {
+        return 0;
+    }
 
-	if (right->left == 0) {
-		right = right->right;
-		ri->right = 0;
-		return ri;
-	}
+    if (c->left == 0) {
+        this->right = c->right;
+        if (c->right) {
+            c->right->parent = this;
+        }
+        c->right = 0;
+        c->parent = 0;
+        return c;
+    }
 
-	Node<T> *parent, *child;
-	parent = right;
-	child = right->left;
-	while (child->left != 0) {
-		parent = child;
-		child = child->left;
-	}
-	parent->left = child->right;
-	child->right = 0;
-	return child;
+    while (c->left != 0) {
+        p = c;
+        c = c->left;
+    }
+    p->left = c->right;
+    if (p->left != 0) {
+        p->left->parent = p;
+    }
+    c->right = 0;
+    c->parent = 0;
+    return c;
 }
 
 template <class T>
-void Node<T>::removeChilds() {
-	if (left != 0) {
-		left->removeChilds();
-		delete left;
-		left = 0;
-	}
-	if (right != 0) {
-		right->removeChilds();
-		delete right;
-		right = 0;
-	}
+Node<T>* Node<T>::rot_right(Node<T>* x) {
+    Node<T> *y = x->left;
+    x->left = y->right;
+    if (y->right)
+        y->right->parent = x;
+    y->right = x;
+    y->parent = x->parent;
+    x->parent = y;
+    if (y->parent) {
+        if (y->parent->left == x)
+            y->parent->left = y;
+        else
+            y->parent->right = y;
+    }
+    return y;
 }
 
 template <class T>
-Node<T>* Node<T>::rot_right(Node<T>* x){
-	Node<T> *y = x->left;
-	x->left = y->right;
-	if(y->right)
-		y->right->parent = x;
-	y->right = x;
-	// enlace de y con el padre de x
-	y->parent = x->parent;
-	x->parent = y;
-	// hace enlace del padre de x a y
-	if(y->parent){
-		if(y->parent->left && y->parent->left->value == x->value)
-			y->parent->left = y;
-		else
-			y->parent->right = y;
-	}
-	return y;
+Node<T>* Node<T>::rot_left(Node<T>* x) {
+    Node<T> *y = x->right;
+    x->right = y->left;
+    if (y->left)
+        y->left->parent = x;
+    y->left = x;
+    y->parent = x->parent;
+    x->parent = y;
+    if (y->parent) {
+        if (y->parent->left == x)
+            y->parent->left = y;
+        else
+            y->parent->right = y;
+    }
+    return y;
 }
 
 template <class T>
-Node<T>* Node<T>::rot_left(Node<T>* x){
-	Node<T> *y = x->right;
-	x->right = y->left;
-	if(y->left)
-		y->left->parent = x;
-	y->left = x;
-	// enlace de y con el padre de x
-	y->parent = x->parent;
-	x->parent = y;
-	// hace enlace del padre de x a y
-	if(y->parent){
-		if(y->parent->left && y->parent->left->value == x->value)
-			y->parent->left = y;
-		else
-			y->parent->right = y;
-	}
-	return y;
+Node<T>* Node<T>::splay(Node<T>* root, Node<T>* x) {
+    if (x == 0) return root;
+
+    while (x->parent != 0) {
+        if (x->parent->parent == 0) {
+            if (x->parent->left == x) {
+                rot_right(x->parent);
+            } else {
+                rot_left(x->parent);
+            }
+        } else {
+            Node<T> *p = x->parent;
+            Node<T> *g = p->parent;
+
+            if (p->left == x && g->left == p) {
+                rot_right(g);
+                rot_right(p);
+            } else if (p->right == x && g->right == p) {
+                rot_left(g);
+                rot_left(p);
+            } else if (p->left == x && g->right == p) {
+                rot_right(p);
+                rot_left(g);
+            } else {
+                rot_left(p);
+                rot_right(g);
+            }
+        }
+    }
+    return x;
 }
 
 template <class T>
-Node<T>* Node<T>::splay(Node<T>* root, Node<T>* x){
-	while(x->parent != 0){
-		if(x->parent->value == root->value){
-			if(x->parent->left && x->parent->left->value == x->value){
-				rot_right(x->parent);
-			}else{
-				rot_left(x->parent);
-			}
-		}else{
-			Node<T>*p = x->parent; // parent
-			Node<T>*g = p->parent; // granparent
-			if(p->left && g->left &&
-				x->value == p->left->value && p->value == g->left->value){
-				rot_right(g);
-				rot_right(p);
-			}else if(p->right && g->right &&
-				x->value == p->right->value && p->value == g->right->value){
-				rot_left(g);
-				rot_left(p);
-			}else	if(p->left && g->right &&
-				x->value == p->left->value && p->value == g->right->value){
-				rot_right(p);
-				rot_left(g);
-			}else{
-				rot_left(p);
-				rot_right(g);
-			}
-		}
-	}
-	return x;
+Node<T>* Node<T>::remove(T val) {
+    Node<T> *p = 0;
+    if (val < value) {
+        if (left != 0) {
+            if (left->value == val) {
+                Node<T> *old = left;
+                p = this;
+
+                if (old->left == 0 || old->right == 0) {
+                    Node<T> *child = (old->left != 0) ? old->left : old->right;
+                    left = child;
+                    if (child) child->parent = this;
+                } else {
+                    Node<T> *succ = old->succesor(); 
+                    left = succ;
+                    succ->left = old->left;
+                    if (succ->left) succ->left->parent = succ;
+                    succ->right = old->right;
+                    if (succ->right) succ->right->parent = succ;
+                    succ->parent = this;
+                    p = succ;
+                }
+
+                delete old;
+                return p;
+            } else {
+                return left->remove(val);
+            }
+        }
+    } else if (val > value) {
+        if (right != 0) {
+            if (right->value == val) {
+                Node<T> *old = right;
+                p = this;
+
+                if (old->left == 0 || old->right == 0) {
+                    Node<T> *child = (old->left != 0) ? old->left : old->right;
+                    right = child;
+                    if (child) child->parent = this;
+                } else {
+                    Node<T> *succ = old->succesor();
+                    right = succ;
+                    succ->left = old->left;
+                    if (succ->left) succ->left->parent = succ;
+                    succ->right = old->right;
+                    if (succ->right) succ->right->parent = succ;
+                    succ->parent = this;
+                    p = succ;
+                }
+
+                delete old;
+                return p;
+            } else {
+                return right->remove(val);
+            }
+        }
+    }
+    return this;
 }
 
 template <class T>
 void Node<T>::inorder(std::stringstream &aux) const {
-	if (left != 0) {
-		left->inorder(aux);
-	}
-	if (aux.tellp() != 1) {
-		aux << " ";
-	}
-	aux << value;
-	if (right != 0) {
-		right->inorder(aux);
-	}
-}
-
-template <class T>
-void Node<T>::print_tree(std::stringstream &aux) const {
-	if (parent != 0){
-		aux << "\n node " << value;
-		aux << " parent " << parent->value;
-	}else
-		aux << "\n root " << value;
-	if (left != 0)
-		aux << " left " << left->value;
-	if (right != 0)
-		aux << " right " << right->value;
-	aux << "\n";
-
-	if (left != 0) {
-		left->print_tree(aux);
-	}
-	if (right != 0) {
-		right->print_tree(aux);
-	}
+    if (left != 0) {
+        left->inorder(aux);
+        aux << " ";
+    }
+    aux << value;
+    if (right != 0) {
+        aux << " ";
+        right->inorder(aux);
+    }
 }
 
 template <class T>
 void Node<T>::preorder(std::stringstream &aux) const {
-	aux << value;
-	if (left != 0) {
-		aux << " ";
-		left->preorder(aux);
-	}
-	if (right != 0) {
-		aux << " ";
-		right->preorder(aux);
-	}
+    aux << value;
+    if (left != 0) {
+        aux << " ";
+        left->preorder(aux);
+    }
+    if (right != 0) {
+        aux << " ";
+        right->preorder(aux);
+    }
 }
 
 template <class T>
 class SplayTree {
 private:
-	Node<T> *root;
+    Node<T> *root;
 
 public:
-	SplayTree();
-	~SplayTree();
-	bool empty() const;
-	void add(T);
-	bool find(T);
-	void remove(T);
-	void removeAll();
-	std::string inorder() const;
-	std::string print_tree() const;
-	std::string preorder() const;
+    SplayTree();
+    ~SplayTree();
+    bool empty() const;
+    void add(T);
+    bool find(T);
+    void remove(T);
+    std::string inorder() const;
+    std::string preorder() const;
 };
 
 template <class T>
@@ -260,122 +284,95 @@ SplayTree<T>::SplayTree() : root(0) {}
 
 template <class T>
 SplayTree<T>::~SplayTree() {
-	removeAll();
 }
 
 template <class T>
 bool SplayTree<T>::empty() const {
-	return (root == 0);
+    return (root == 0);
 }
 
 template<class T>
 void SplayTree<T>::add(T val) {
-	if (root != 0) {
-			Node<T>* added = root->add(val);
-			root = root->splay(root,added);
-	} else {
-		root = new Node<T>(val);
-	}
+    if (root != 0) {
+        Node<T>* added = root->add(val);
+        root = root->splay(root, added);
+    } else {
+        root = new Node<T>(val);
+    }
+}
+
+template <class T>
+bool SplayTree<T>::find(T val) {
+    if (root != 0) {
+        Node<T>* found = root->find(val);
+        root = root->splay(root, found);
+        return (root->value == val);
+    } else {
+        return false;
+    }
 }
 
 template <class T>
 void SplayTree<T>::remove(T val) {
-    if (root == 0) {
-        return; 
-    }
+    if (root != 0) {
+        if (val == root->value) {
+            Node<T> *old = root;
+            Node<T> *succ = root->succesor(); 
 
-    Node<T>* p = root->find(val);
-    if (p == 0) {
-        return;
-    }
-    root = root->splay(root, p);
-    if (root->value != val) {
-        return; 
-    }
-	
-    Node<T> *left_subtree = root->left;
-    Node<T> *right_subtree = root->right;
-    if (left_subtree) left_subtree->parent = 0;
-    if (right_subtree) right_subtree->parent = 0;
-    delete root;
+            if (succ != 0) {
+                succ->left = old->left;
+                if (succ->left)
+                    succ->left->parent = succ;
 
-    if (left_subtree == 0) {
-        root = right_subtree;
-        if (root) root->parent = 0; 
-    } else {
-        Node<T> *new_root_candidate = left_subtree;
-        while (new_root_candidate->right != 0) {
-            new_root_candidate = new_root_candidate->right;
+                succ->right = old->right;
+                if (old->right == succ) { 
+                } else if (succ->right) {
+                    succ->right->parent = succ;
+                } else {
+                    succ->right = old->right;
+                    if (succ->right)
+                        succ->right->parent = succ;
+                }
+
+                succ->parent = 0;
+            } else if (root->left) { 
+                succ = root->left;
+                succ->parent = 0;
+            }
+
+            delete old;
+            root = succ;
+
+        } else {
+            Node<T>* p = root->remove(val);
+            
+            if (p != root) { 
+                root = root->splay(root, p);
+            }
         }
-
-        Node<T>* new_root = new_root_candidate->splay(left_subtree, new_root_candidate);
-        root = new_root;
-        
-        root->right = right_subtree;
-        if (right_subtree) {
-            right_subtree->parent = root;
-        }
     }
-}
-
-template <class T>
-void SplayTree<T>::removeAll() {
-	if (root != 0) {
-		root->removeChilds();
-	}
-	delete root;
-	root = 0;
-}
-
-template <class T>
-bool SplayTree<T>::find(T val){
-	if (root != 0) {
-		Node<T>* found = root->find(val);
-        if (found != 0){
-		    root = root->splay(root,found);
-            return true;
-        }
-		return false;
-	}
-	return false;
 }
 
 template <class T>
 std::string SplayTree<T>::inorder() const {
-	std::stringstream aux;
-
-	aux << "[";
-	if (!empty()) {
-		root->inorder(aux);
-	}
-	aux << "]";
-	return aux.str();
+    std::stringstream aux;
+    aux << "[";
+    if (!empty()) {
+        root->inorder(aux);
+    }
+    aux << "]";
+    return aux.str();
 }
-
-template <class T>
-std::string SplayTree<T>::print_tree() const {
-	std::stringstream aux;
-
-	aux << "\n ================================ ";
-	if (!empty()) {
-		root->print_tree(aux);
-	}
-	aux << " ================================ \n";
-	return aux.str();
-}
-
-
 
 template <class T>
 std::string SplayTree<T>::preorder() const {
-	std::stringstream aux;
-
-	aux << "[";
-	if (!empty()) {
-		root->preorder(aux);
-	}
-	aux << "]";
-	return aux.str();
+    std::stringstream aux;
+    aux << "[";
+    if (!empty()) {
+        root->preorder(aux);
+    }
+    aux << "]";
+    return aux.str();
 }
 
-#endif /* SplayTree_H_ */
+#endif /* SPLAY_H_ */
